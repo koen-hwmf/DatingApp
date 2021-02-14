177822d7fad997dc77cfc3cf094f9ff200658559
Koen <koenvanstraalen@gmail.com>
2021-02-14 15:35:25 +0100

    End of section 8

---
+++ API/Data/Seed.cs
@@ -0,0 +1,41 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public class Seed
+    {
+        public static async Task SeedUsers(DataContext context)
+        {
+            // Check if we already have users in database and return if true
+            if (await context.Users.AnyAsync()) return;
+            
+            // Get userdata from json file
+            var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
+
+            // Create user objects from userData
+            var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
+
+            foreach (var user in users)
+            {
+                using var hmac = new HMACSHA512();
+
+                user.UserName = user.UserName.ToLower(); // set username
+                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Pa$$w0rd")); // create passwordhash
+                user.PasswordSalt = hmac.Key; // create passwordsalt
+
+                // add tracking to user through Entity framework
+                context.Users.Add(user);
+            }
+
+            await context.SaveChangesAsync();
+        } 
+    }
+}
---
+++ API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static int CalculateAge(this DateTime dob)  
+        {
+            // get current date
+            var today = DateTime.Today;
+            // calculate age by subtracting birth year from current year
+            var age = today.Year - dob.Year;
+            // check if birthday is yet to come this year or not and adjust age accordingly
+            if (dob.Date > today.AddYears(-age)) age--;
+            return age;
+        }
+    }
+}
